33d4dd8bc82bcc2df4a42fa5f4a4d209f2ed0f62
ulxlx <ulxlx71@gmail.com>
2017-09-15 19:53:57 -0400

    more menu work

--- game.py
+++ game.py
@@ -1,11 +1,20 @@
 import pygame
 import math
+import eztext
+import socket
 
 from bullet import bullet
+from datetime import *
 
 pygame.init()
+pygame.font.init()
+
 width, height = 1280, 800
 screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+myfont = pygame.font.SysFont('Arial', 30)
+
+txtbx = eztext.Input(maxlength=45, color=(255,0,0), prompt='Enter your name: ')
 
 ship_img = pygame.image.load('img/ship.jpg')
 ship_w = ship_img.get_rect().size[0]
@@ -15,45 +24,65 @@ ship_h = ship_img.get_rect().size[1]
 playerx, playery = 0,0
 playerx_a, playery_a = 0, 0
 playerx_v, playery_v = 0, 0
+playername = ''
 
 bullets = []
 
-max_v = 15
+max_v = 8
 
 exit = False
-name_entry = False
+name_entry = True
 while name_entry:
 	events = pygame.event.get()
 	for event in events:
 		if event.type == pygame.QUIT:
 			name_entry = False
 			exit = True
+	keys=pygame.key.get_pressed()
+	if keys[pygame.K_RETURN]:
+		playername = txtbx.value
+		name_entry = False
+
+	txtbx.update(events)
+	txtbx.draw(screen)
+	pygame.display.flip()
+	clock.tick(30)
 
 if not exit:
-	menu = False
+	screen.fill((0,0,0))
+
+	textsurface = myfont.render('Waiting for an opponent...', False, (255, 255, 255))
+	screen.blit(textsurface,(width/2, height/2))
+	pygame.display.flip()
+	s = socket.socket()
+	host = socket.gethostname()
+	port = 8888
+	s.connect((host, port))
+
+	menu = True
 	while menu:
 		events = pygame.event.get()
 		for event in events:
 			if event.type == pygame.QUIT:
 				menu = False
 				exit = True
-
+		clock.tick(30)
 
 if not exit:
 	playing = True
 	while playing:  
 		keys=pygame.key.get_pressed()
 		if keys[pygame.K_a]:
-			playerx_a = -3
+			playerx_a = -1
 		elif keys[pygame.K_d]:
-			playerx_a = 3
+			playerx_a = 1
 		else:
 			playerx_a = 0
 
 		if keys[pygame.K_w]:
-			playery_a = -3
+			playery_a = -1
 		elif keys[pygame.K_s]:
-			playery_a = 3
+			playery_a = 1
 		else:
 			playery_a = 0
 
@@ -109,3 +138,5 @@ if not exit:
 		ship_img_r = pygame.transform.rotate(ship_img, angle)
 		screen.blit(ship_img_r, (playerx, playery))
 		pygame.display.flip()   
+		clock.tick(60)
+s.close()
